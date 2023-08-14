33369f57bce2fd92ab0bbd97112f22ff6911aba7
haml-707 <845831435@qq.com>
2023-08-14 16:04:22 +0800

    init web-ui

---
+++ web-ui/docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="euler-app">
+    <CustomHeader />
+    <Content v-if="!isPost()" class="content" :class="{ 'is-home': $route.path.length === 4 }"/>
+    <Post v-if="isPost()" class="content" />
+    <CustomFooter />
+  </div>
+</template>
+
+<script>
+import CustomHeader from "@theme/components/CustomHeader.vue";
+import CustomFooter from "@theme/components/CustomFooter.vue";
+import Post from "@theme/layouts/Post.vue";
+
+export default {
+  name: "Layout",
+
+  components: {
+    CustomHeader,
+    CustomFooter,
+    Post
+  },
+
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+  methods: {
+    isPost() {
+      let currentPath = this.$route.path;
+      return currentPath.indexOf("/" + this.$lang + "/blog/") > -1
+        ? true
+        : false;
+    }
+  },
+};
+</script>
+<style lang="less" scoped>
+.content {
+    min-height: calc(100vh - 178px);
+    @media (max-width: 1000px) {
+        min-height: calc(100vh - 70px - 328px);
+    }
+}
+.is-home {
+  background-color:#f5f6f8;
+}
+
+</style>
